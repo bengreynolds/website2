c061887e0a05fb74e8955c8ed626b4fc019d5cc9
Ben Reynolds <Benjamin.g.reynolds@ucdenver.edu>
2026-09-10 16:56:46 -0600

    Turn the rail into a depth gauge for where the reader is standing

--- src/main.jsx
+++ src/main.jsx
@@ -30,6 +30,7 @@ import "./demo-lickrevolver-trial-close.css";
 import "./demo-lickrevolver-ui.css";
 import "./demo-reach-single.css";
 import "./demo-reach-session.css";
+import "./rail-depth.css";
 import "./pipeline-demo.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
